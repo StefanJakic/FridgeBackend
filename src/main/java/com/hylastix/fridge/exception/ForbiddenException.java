dc06877368ea43f8600ef869867ba81dc12bdc78
Stefan Jakic <sjakic20@gmail.com>
2025-07-19 09:55:14 +0200

    Added project structure and global exception

---
+++ src/main/java/com/hylastix/fridge/exception/ForbiddenException.java
@@ -0,0 +1,7 @@
+package com.hylastix.fridge.exception;
+ 
+public class ForbiddenException extends RuntimeException {
+    public ForbiddenException(String message) {
+        super(message);
+    }
+} 
